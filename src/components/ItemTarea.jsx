473ae3d7ee4cebb1dcdaf51ba74e576b4945cb6a
matias <pagosptcmatias@gmail.com>
2022-09-22 17:40:15 -0300

    agregamos el maquetado a los componentes formulario, lista e item

---
+++ src/components/ItemTarea.jsx
@@ -0,0 +1,13 @@
+import { Button, ListGroup} from "react-bootstrap";
+const ItemTarea = () => {
+    return (
+        
+          <ListGroup.Item className="d-flex justify-content-between">
+            Tarea de Prueba 
+            <Button variant="danger">Borrar</Button>
+            </ListGroup.Item>
+       
+    );
+};
+
+export default ItemTarea;
